a807a326afd161072d0f2cbafbcf817b4424c9f2
Ahmad Saleem <hafizahmadsaleem13119@gmail.com>
2026-09-15 12:50:03 -0700

    done

--- users/models.py
+++ users/models.py
@@ -1,19 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Artist(models.Model):
+class Artist(AbstractUser):
     
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        related_name="artist",
-        null=True,
-        blank=True
-    )
-
-    name = models.CharField(max_length=20)
     bio = models.TextField(max_length=100)
     image = models.ImageField(upload_to="artists/", blank=True, null=True)
 
